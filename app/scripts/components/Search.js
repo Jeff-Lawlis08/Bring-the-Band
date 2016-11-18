0cc49df106d1805560d174a0f71d9672c3a623df
Jeff Lawlis <jeff.lawlis@yahoo.com>
2016-11-17 19:15:29 -0600

    added nav

--- app/scripts/components/Search.js
+++ app/scripts/components/Search.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Nav from './nav';
 
 export default React.createClass({
   render(){
     return (
       <div>
+      <Nav/>
         <input type="text" placeholder="search"/>
         <input type="search" value="search"/>
       </div>
